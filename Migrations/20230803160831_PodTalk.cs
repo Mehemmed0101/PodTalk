4c8fbe19938853202a13f4e4eac18d7038f65c11
Mehemmed0101 <mahammadag@code.edu.az>
2023-08-03 21:06:42 +0400

    PodTalk

---
+++ Migrations/20230803160831_PodTalk.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PodTalk.Migrations
+{
+    public partial class PodTalk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Footer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LogoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImgUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DesignedBy = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Footer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SocialMedia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FooterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SocialMedia", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SocialMedia_Footer_FooterId",
+                        column: x => x.FooterId,
+                        principalTable: "Footer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SocialMedia_FooterId",
+                table: "SocialMedia",
+                column: "FooterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "SocialMedia");
+
+            migrationBuilder.DropTable(
+                name: "Footer");
+        }
+    }
+}
---
+++ ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PodTalk.DataAccessLayer;
+
+namespace PodTalk.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _dbContext;
+
+        public FooterViewComponent(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var footer = _dbContext.Footer.Include(x => x.SocialMedia).FirstOrDefault();
+
+            return View(footer);
+        }
+    }
+}
